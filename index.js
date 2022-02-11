f51100847344069aadf00b87ac38c9cf409df89d
pedrorhamon <pedrorhamon16@gmail.com>
2022-02-10 23:34:11 -0300

    Projeto criado, com o cenario incompleto

---
+++ index.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerWidth;
+
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        };
+        this.width = 30;
+        this.height = 30;
+    }
+    draw() {
+        c.fillStyle = 'green';
+        c.fillRect(
+            this.position.x,
+             this.position.y,
+              this.width,
+              this.height
+              );
+    }
+}
+
+const player = new Player();
+player.draw();
