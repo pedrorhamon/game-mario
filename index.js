890dda54cde0f1e8282c260a63e4f92c0c1ee50f
pedrorhamon <pedrorhamon16@gmail.com>
2022-02-12 12:46:06 -0300

    created gravity

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerWidth;
 
-
+const gravity = 0.5;
 class Player {
     constructor() {
         this.position = {
             x: 100,
             y: 100
+        };
+        this.velocity = {
+            x: 0,
+            y: 0
         };
         this.width = 30;
         this.height = 30;
@@ -18,12 +22,27 @@ class Player {
         c.fillStyle = 'green';
         c.fillRect(
             this.position.x,
-             this.position.y,
-              this.width,
-              this.height
-              );
+            this.position.y,
+            this.width,
+            this.height
+        );
+    }
+    update() {
+        this.draw()
+        this.position.y += this.velocity.y;
+
+        if (this.position.y + this.height + 
+            this.velocity.y <= canvas.height) 
+            this.velocity.y += gravity;
+        else this.velocity.y = 0;
     }
 }
 
 const player = new Player();
 player.draw();
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.update();
+}
