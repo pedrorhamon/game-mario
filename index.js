5d255fb60f8aea42aef1f74e691d029a67c47a9d
pedrorhamon <pedrorhamon16@gmail.com>
2022-02-12 13:35:42 -0300

    Player moviment

--- index.js
+++ index.js
@@ -29,20 +29,77 @@ class Player {
     }
     update() {
         this.draw()
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
-        if (this.position.y + this.height + 
-            this.velocity.y <= canvas.height) 
+        if (this.position.y + this.height +
+            this.velocity.y <= canvas.height)
             this.velocity.y += gravity;
         else this.velocity.y = 0;
     }
 }
 
 const player = new Player();
-player.draw();
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    },
+}
 
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update();
+    if (keys.right.pressed) {
+        player.velocity.x = 5;
+    } else if (keys.left.pressed) {
+        player.velocity.x = -5;
+    } else {
+        player.velocity.x = 0;
+    }
 }
+
+animate()
+
+addEventListener('keydown', ({ keyCode }) => {
+    switch (keyCode) {
+        case 65:
+            console.log('left');
+            keys.left.pressed = true;
+            break
+        case 83:
+            console.log('down');
+            break
+        case 68:
+            console.log('right');
+            keys.right.pressed = true;
+            break
+        case 87:
+            console.log('up');
+            player.velocity.y -= 20;
+            break
+    }
+})
+
+addEventListener('keyup', ({ keyCode }) => {
+    switch (keyCode) {
+        case 65:
+            console.log('left');
+            keys.left.pressed = false;
+            break
+        case 83:
+            console.log('down');
+            break
+        case 68:
+            console.log('right');
+            keys.right.pressed = false;
+            break
+        case 87:
+            console.log('up');
+            player.velocity.y -= 20;
+            break
+    }
+})
